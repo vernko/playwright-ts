a0734ffd551eb27a1af559e65b85cd45bdcaa409
Vern Kofford <vernkofford@Verns-MBP.localdomain>
2025-11-04 23:14:24 -0700

    Resolves #6
    Created new page object page for radiography. Created spec folder to house all spec files. Modified other pages needed for tests

--- tests/mtech/pages/school-of-healthcare.ts
+++ tests/mtech/pages/school-of-healthcare.ts
@@ -2,6 +2,12 @@ import { expect, type Locator, type Page } from "@playwright/test"
 
 export class SchoolOfHealthcarePage {
     get schoolOfHealthcareHeading() { return this.page.locator('[data-id="ce34e5b"]') }
+    get radiographyTechnologyWidget() { return this.page.locator('#post-141853') }
 
     constructor (readonly page: Page) {}
+
+    // think abour abstracting this function to a general use function
+    async clickHealthProgram(program: Locator) {
+        await program.click()
+    }
 }
